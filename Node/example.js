dcf3d6fedd57e66d0b3bd94e03e34a1c9279fa95
Parrichaya <parrichaya.ecec@gmail.com>
2024-02-10 18:39:06 +0530

    js_practice

--- Node/example.js
+++ Node/example.js
@@ -1,6 +1,49 @@
-const name = 'Parichay'
-const age = 32
-const address = 'Dehradun'
-console.log(`My name is ${name},age is ${age} and address is ${address}`)
-let str = 'My name is Parichay,age is 32,address is Dehradun'
-console.log(str.split(','))
+// Class and Inheritance
+
+class user {                                    // user class
+    static count                                // static variable
+    constructor(username,email,password) {      // constructor for user class
+        this.username = username
+        this.email = email
+        this.password = password
+        user.count++
+    }
+
+    printNumberOfUsers() {                      // class method
+        console.log("The number of users = ",user.count)
+    }
+}
+/**************************************************************************************** */
+class member extends user {                     // new member class inheriting user class
+    constructor(username,email,password) {
+        super(username,email,password)
+        this.membershipactivetilldate = new Date(2024,2,8)
+        this.package = ''
+    }
+
+    purchaseMembership(membershippackagename) {         // class method
+        if(membershippackagename === 'MONTHLYPACKAGE') {
+          this.membershipactivetilldate.setMonth(this.membershipactivetilldate.getMonth() + 1)
+          this.package = 'MONTHLYPACKAGE'
+        } else if(membershippackagename === 'YEARLYPACKAGE') {
+          this.membershipactivetilldate.setFullYear(this.membershipactivetilldate.getFullYear() + 1)
+          this.package = 'YEARLYPACKAGE'
+        }
+      }
+
+    subscriptionActiveTill() {                          // class method
+        console.log(this.username + " is subscribed to " + this.package + " uptill " + this.membershipactivetilldate.toDateString())
+    }
+}    
+/***************************************************************************************** */
+function createNewStudents(username, email, password, membershippackagename) {              // function
+    const mike = new member(username, email, password);  
+    mike.purchaseMembership(membershippackagename);
+    mike.subscriptionActiveTill();
+}
+
+let membershippackagename = 'MONTHLYPACKAGE'
+let username = 'Mohan'
+let email = 'mohan@gmail.com'
+let password = 'fgfggdj'
+createNewStudents(username, email, password, membershippackagename)
