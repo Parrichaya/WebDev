065b369dd86119fcadbab1b7cf0f7c05655506dc
Parrichaya <parrichaya.ecec@gmail.com>
2024-02-13 00:11:32 +0530

    basicChanges

--- Node/addDelFruits.js
+++ Node/addDelFruits.js
@@ -8,14 +8,26 @@ for (let i = 0; i < fruitList.length; i++) {
 const form = document.querySelector('form')
 const fruits = document.querySelector('.fruits')
 
+const fruitDescription = document.createElement('input')
+fruitDescription.type = 'text'
+fruitDescription.id = 'description'
+form.insertBefore(fruitDescription,form.querySelector('button'))
+
 form.addEventListener('submit', function(event){
     event.preventDefault()
-    const fruitsToAdd = document.getElementById('fruit-to-add')
+    const fruitToAdd = document.getElementById('fruit-to-add')
+    const descToAdd = document.getElementById('description') 
 
     const newLi = document.createElement('li')
-    newLi.innerHTML = fruitsToAdd.value + '<button class="delete-btn">x</button>' + '<button class="edit-btn">Edit</button>' 
+    newLi.innerHTML = fruitToAdd.value + '<button class="delete-btn">x</button>' + '<button class="edit-btn">Edit</button>'
+    
+    const newP = document.createElement('p')
+    newP.innerHTML = descToAdd.value
+    newLi.appendChild(newP).style.fontStyle = 'italic'
+
     fruits.appendChild(newLi)
 })
+
 
 fruits.addEventListener('click', function(event){
     if(event.target.classList.contains('delete-btn')){
@@ -23,3 +35,23 @@ fruits.addEventListener('click', function(event){
         fruits.removeChild(fruitToDelete)
     }
 })
+
+// filter functionality
+const filter = document.getElementById('filter')
+
+filter.addEventListener('keyup',function(event) {
+    const textEntered = event.target.value.toLowerCase()
+    const fruitItems = document.getElementsByClassName('fruit')
+    for (let i = 0; i < fruitItems.length; i++) {
+        const currentFruitText = fruitItems[i].firstChild.textContent.toLowerCase()
+        const currentFruitDescription = fruitItems[i].getElementsByTagName('p')[0].textContent.toLowerCase()
+        if (currentFruitText.indexOf(textEntered) === -1 && currentFruitDescription.indexOf(textEntered) === -1) {
+            fruitItems[i].style.display = "none"
+        }
+        else {
+            fruitItems[i].style.display = "flex"
+        }
+    }
+})
+
+
--- Node/patient_form.js
+++ Node/patient_form.js
@@ -1,8 +1,19 @@
+// function printform(event) {
+//     event.preventDefault()
+//     console.log(event.target.name.value)
+//     console.log(event.target.email.value)
+//     console.log(event.target.phone.value)
+//     console.log(event.target.date.value)
+//     console.log(event.target.time.value)
+//   }
 function printform(event) {
-    event.preventDefault()
-    console.log(event.target.name.value)
-    console.log(event.target.email.value)
-    console.log(event.target.phone.value)
-    console.log(event.target.date.value)
-    console.log(event.target.time.value)
-  }
+  event.preventDefault()
+  
+  const name = event.target.name.value
+  const email = event.target.email.value
+  const phone = event.target.phone.value
+
+  localStorage.setItem('name', name)
+  localStorage.setItem('email', email)
+  localStorage.setItem('phone', phone)
+}
