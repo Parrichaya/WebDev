24aca79d6e8467a6354eb55a1777f4d4c3424f39
Parrichaya <parrichaya.ecec@gmail.com>
2024-03-20 19:35:29 +0530

    request and response

--- BackendWithNodeJS/app.js
+++ BackendWithNodeJS/app.js
@@ -1,7 +1,28 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-    console.log('Parichay Negi');
+    // console.log('Parichay Negi');
+    if (req.url === '/home') {
+        res.setHeader('Content-Type', 'text/html');
+        res.write('<html>');
+        res.write('<body><h1>Welcome home</h1></body>');
+        res.write('</html>');
+        res.end();
+    }
+    else if (req.url === '/about') {
+        res.setHeader('Content-Type', 'text/html');
+        res.write('<html>');
+        res.write('<body><h1>Welcome to About Us page</h1></body>');
+        res.write('</html>');
+        res.end();
+    }
+    else if (req.url === '/node') {
+        res.setHeader('Content-Type', 'text/html');
+        res.write('<html>');
+        res.write('<body><h1>Welcome to my Node Js project</h1></body>');
+        res.write('</html>');
+        res.end();
+    }
 })
 
 server.listen(4000, () => {
