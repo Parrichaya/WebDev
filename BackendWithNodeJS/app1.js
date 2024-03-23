6fde29a8d27e3d61dbadea824354fc8501b7702f
Parrichaya <parrichaya.ecec@gmail.com>
2024-03-24 01:34:14 +0530

    Routers and filters added

--- BackendWithNodeJS/app1.js
+++ BackendWithNodeJS/app1.js
@@ -1,27 +1,20 @@
-const http = require('http');
-// const routes = require('./routes.js')
-const bodyParser = require('body-parser');
-
 const express = require('express');
 
 const app = express();
 
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
+const bodyParser = require('body-parser');
+
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use('/add-product', (req, res, next) => {
-  res.send('<form action="/product" method="POST"><input type="text" name="title"><br>'+
-  '<input type="text" name="size"><br>' +'<button type="submit">Add Product</button></form>');
-});
+app.use('/admin',adminRoutes);
+app.use('/shop',shopRoutes);
 
-app.use('/product', (req, res, next) => {
-  console.log(req.body);
-  res.redirect('/');
-});
-
-app.use('/', (req, res, next) => {
-  console.log('In another middleware');
-  res.send('<h1>Hello from Express!</h1>');
-});
+app.use((req, res, next) => {
+  res.status(404).send('<h1>Page not found</h1>');
+})
 
 app.listen(4000, () => {
   console.log('Listening...');
