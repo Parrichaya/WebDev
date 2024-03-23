b3ac3b856c9d76042194f8bc5adbae868650bb15
Parrichaya <parrichaya.ecec@gmail.com>
2024-03-24 00:26:20 +0530

    Bodyparser added

--- BackendWithNodeJS/app1.js
+++ BackendWithNodeJS/app1.js
@@ -1,16 +1,24 @@
 const http = require('http');
 // const routes = require('./routes.js')
+const bodyParser = require('body-parser');
 
 const express = require('express');
 
 const app = express();
 
-app.use((req, res, next) => {
-  console.log('In the middleware');
-  next();
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use('/add-product', (req, res, next) => {
+  res.send('<form action="/product" method="POST"><input type="text" name="title"><br>'+
+  '<input type="text" name="size"><br>' +'<button type="submit">Add Product</button></form>');
 });
 
-app.use((req, res, next) => {
+app.use('/product', (req, res, next) => {
+  console.log(req.body);
+  res.redirect('/');
+});
+
+app.use('/', (req, res, next) => {
   console.log('In another middleware');
   res.send('<h1>Hello from Express!</h1>');
 });
