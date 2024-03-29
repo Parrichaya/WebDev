bdddfc93329db4c6e02d8182b297167a77186aa2
Parrichaya <parrichaya.ecec@gmail.com>
2024-03-29 13:05:39 +0530

    Sequelize added to work with the database

--- starting-setup3/models/product.js
+++ starting-setup3/models/product.js
@@ -1,42 +1,26 @@
-const fs = require('fs');
-const path = require('path');
+const Sequelize = require('sequelize');
 
-const db = require('../util/database');
+const sequelize = require('../util/database');
 
-const Cart = require('./cart');
-
-const p = path.join(
-  path.dirname(process.mainModule.filename),
-  'data',
-  'products.json'
-);
-
-
-
-module.exports = class Product {
-  constructor(id, title, imageUrl, description, price) {
-    this.id = id;
-    this.title = title;
-    this.imageUrl = imageUrl;
-    this.description = description;
-    this.price = price;
+const Product = sequelize.define('product', {
+  id: {
+    type: Sequelize.INTEGER,
+    autoIncrement: true,
+    allowNull: false,
+    primaryKey: true
+  },
+  title: Sequelize.STRING,
+  price: {
+    type: Sequelize.DOUBLE,
+    allowNull: false
+  },
+  imageUrl: {
+    type:Sequelize.STRING,
+    allowNull: false},
+  description: {
+    type: Sequelize.STRING,
+    allowNull: false
   }
+})
 
-// save to database
-  save() {
-    return db.execute('INSERT INTO products (title, price, description, imageUrl) VALUES (?, ?, ?, ?)', 
-    [this.title, this.price, this.imageUrl, this.description]);
-  }
-
-  static deleteById(id) {
-    return db.execute('DELETE FROM products WHERE products.id = ?', [id]);
-  }
-
-  static fetchAll() {
-    return db.execute('SELECT * FROM products')
-  }
-
-  static findById(id) {
-    return db.execute('SELECT * FROM products WHERE products.id = ?', [id]);
-  }
-};
+module.exports = Product;
