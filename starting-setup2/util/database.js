2bdc3c04afcff671bad76275877402ed4159900e
Parrichaya <parrichaya.ecec@gmail.com>
2024-04-15 12:17:54 +0530

    removing duplicates

---
+++ starting-setup2/util/database.js
@@ -0,0 +1,10 @@
+const mysql = require('mysql2');
+
+const pool = mysql.createPool({
+    host: 'localhost',
+    user: 'root',
+    database: 'node-complete',
+    password: 'a2c3b14d4'
+})
+
+module.exports = pool.promise();
