7ee0b8b65b245f70ce30305e45791f22d71b8709
Parrichaya <parrichaya.ecec@gmail.com>
2024-04-15 12:12:15 +0530

    removed duplicate project

--- companyReview/util/database.js
+++
@@ -1,8 +0,0 @@
-const Sequelize = require('sequelize')
-
-const sequelize = new Sequelize('add-review', 'root', 'a2c3b14d4', {
-    dialect: 'mysql',
-    host: 'localhost'
-})
-
-module.exports = sequelize;
