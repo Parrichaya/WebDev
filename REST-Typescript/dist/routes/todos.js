b245c374608f3096795907847b32b929895e8977
Parrichaya <parrichaya.ecec@gmail.com>
2024-04-30 23:19:52 +0530

    type casting and better file structure added

--- REST-Typescript/dist/routes/todos.js
+++ REST-Typescript/dist/routes/todos.js
@@ -1,33 +1,37 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = require("express");
-const todos = [];
 const router = (0, express_1.Router)();
+const todos = [];
 router.get('/', (req, res, next) => {
     res.status(200).json({ todos: todos });
 });
 router.post('/todo', (req, res, next) => {
+    const body = req.body;
     const newTodo = {
         id: new Date().toISOString(),
-        text: req.body.text
+        text: body.text
     };
     todos.push(newTodo);
     res.status(201).json({ message: 'Todo added', todo: newTodo, todos: todos });
 });
 router.put('/todo/:todoId', (req, res, next) => {
-    const tid = req.params.todoId;
+    const params = req.params;
+    const tid = params.todoId;
+    const body = req.body;
     const todoIndex = todos.findIndex(todo => todo.id === tid);
     if (todoIndex >= 0) {
         todos[todoIndex] = {
             id: todos[todoIndex].id,
-            text: req.body.text
+            text: body.text
         };
         return res.status(200).json({ message: 'Updated todo', todos: todos });
     }
     res.status(404).json({ message: 'Could not find todo for this id' });
 });
 router.delete('/todo/:todoId', (req, res, next) => {
-    const tid = req.params.todoId;
+    const params = req.params;
+    const tid = params.todoId;
     const todoIndex = todos.findIndex(todo => todo.id === tid);
     if (todoIndex >= 0) {
         todos.splice(todoIndex, 1);
--- REST-Typescript/src/routes/todos.ts
+++ REST-Typescript/src/routes/todos.ts
@@ -1,31 +1,36 @@
 import {Router} from 'express';
-
 import { Todo } from '../models/todo';
+
+const router = Router();
 
 const todos: Todo[] = []
 
-const router = Router();
+type RequestBody = { text: string }
+type RequestParams = { todoId: string }
 
 router.get('/', (req, res, next) => {
     res.status(200).json({ todos: todos })
 })
 
 router.post('/todo', (req, res, next) => {
+    const body = req.body as RequestBody
     const newTodo: Todo = {
         id: new Date().toISOString(),
-        text: req.body.text
+        text: body.text
     }
     todos.push(newTodo)
     res.status(201).json({message: 'Todo added', todo: newTodo, todos: todos})
 })
 
 router.put('/todo/:todoId', (req, res, next) => {
-    const tid = req.params.todoId
+    const params = req.params as RequestParams
+    const tid = params.todoId
+    const body = req.body as RequestBody
     const todoIndex = todos.findIndex(todo => todo.id === tid)
     if (todoIndex >= 0) {
         todos[todoIndex] = {
             id: todos[todoIndex].id,
-            text: req.body.text
+            text: body.text
         }
         return res.status(200).json({message: 'Updated todo',todos: todos})
     }
@@ -33,7 +38,8 @@ router.put('/todo/:todoId', (req, res, next) => {
 })
 
 router.delete('/todo/:todoId', (req, res, next) => {
-    const tid = req.params.todoId
+    const params = req.params as RequestParams
+    const tid = params.todoId
     const todoIndex = todos.findIndex(todo => todo.id === tid)
     if (todoIndex >= 0) {
         todos.splice(todoIndex, 1)
